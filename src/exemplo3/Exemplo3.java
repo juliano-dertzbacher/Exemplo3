a1d51ad3fdc4555e04a99e5bff5bf213f4bdfc6e
Juliano Dertzbacher <69397499+juliano-dertzbacher@users.noreply.github.com>
2020-08-10 20:22:23 -0300

    Update Exemplo3.java

--- src/exemplo3/Exemplo3.java
+++ src/exemplo3/Exemplo3.java
@@ -15,7 +15,7 @@ public class Exemplo3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.print ("Hello World!!!");
+        System.out.print ("Alô mundo!!!");
     }
     
 }
